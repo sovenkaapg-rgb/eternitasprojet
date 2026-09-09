a2830b90579684befd3056edc9213118ed24676e
SovenkaAPG <sovenkaapg@gmail.com>
2026-09-09 15:28:16 +0300

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -9,7 +9,7 @@ export async function initSiteCounter() {
     const key = "first-counter-5459";
 
     // API-ссылка для автоматического накручивания +1 просмотра при каждом заходе
-    const incrementUrl = `https://counterapi.sovenkaapgs-team-5459/${key}/increment`;
+    const incrementUrl = `https://app.counterapi.dev/team/sovenkaapgs-team`;
 
     let currentCount = "000000"; // Стартовое значение на случай сбоя сети
 
